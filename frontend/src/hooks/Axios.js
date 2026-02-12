87b86ce41b8bb0e479f35535fbce3fe48eadaca6
marcus <62052926+marcuschain-jpg@users.noreply.github.com>
2026-02-12 13:00:29 +0800

    update axios again

--- frontend/src/hooks/Axios.js
+++ frontend/src/hooks/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL:"https://d1poq6hazwmmqe.cloudfront.net:8080",
+    baseURL:"http://15.135.87.40:8080",
     withCredentials: false // by default, will manaully set true on diff calls
 });
 
